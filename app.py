38e584478b02232d95d138359a0a88e2a1b566b8
Idongesit Inyang <inyangidongesit22@gmail.com>
2023-09-15 05:44:23 +0100

    rearranged the source code

--- app.py
+++ app.py
@@ -1,46 +1,43 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
 from flask_migrate import Migrate
 
-class Base(DeclarativeBase):
-  pass
-
-
-db = SQLAlchemy(model_class=Base)
+# Initialize Flask app
 app = Flask(__name__)
 
+# Configure the database URI
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///persons.db'
-db.init_app(app)
+
+# Initialize SQLAlchemy with the app
+db = SQLAlchemy(app)
+
+# Initialize database migration
 migrate = Migrate(app, db)
 
-#define person model
-
+# Define the Person model
 class Person(db.Model):
     __tablename__ = 'persons'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False, unique=True)
-    
 
     def to_json(self):
-        return{
+        return {
             'id': self.id,
             'name': self.name
         }
-    __table_args__ = {'extend_existing':True}
-    
+
+# Create database tables
 with app.app_context():
     db.create_all()
 
+# API routes
 
-#api routes
 @app.route('/api', methods=['POST'])
 def add_person():
     data = request.get_json()
     if 'name' in data:
         name = data['name']
-        new_person = Person(name=data['name'])
-        # Check if a person with the same name already exists
+        new_person = Person(name=name)
         existing_person = Person.query.filter_by(name=name).first()
 
         if existing_person:
@@ -49,16 +46,13 @@ def add_person():
         db.session.commit()
         return jsonify({'message': 'Person added successfully'}), 201
     else:
-        return jsonify({'error': 'Please add name'}), 400
-    
+        return jsonify({'error': 'Please add a name'}), 400
 
-# Retrieve all persons
 @app.route('/api', methods=['GET'])
 def get_all_persons():
     persons = Person.query.all()
     return jsonify([person.to_json() for person in persons])
 
-# Retrieve a specific person by ID
 @app.route('/api/<int:id>', methods=['GET'])
 def get_person(id):
     person = Person.query.get(id)
@@ -67,7 +61,6 @@ def get_person(id):
     else:
         return jsonify({'error': 'Person not found'}), 404
 
-# Update a person by ID
 @app.route('/api/<int:id>', methods=['PUT'])
 def update_person(id):
     person = Person.query.get(id)
@@ -82,7 +75,6 @@ def update_person(id):
     else:
         return jsonify({'error': 'Person not found'}), 404
 
-# Delete a person by ID
 @app.route('/api/<int:id>', methods=['DELETE'])
 def delete_person(id):
     person = Person.query.get(id)
@@ -93,8 +85,5 @@ def delete_person(id):
     else:
         return jsonify({'error': 'Person not found'}), 404
 
-        
-
 if __name__ == '__main__':
-    
     app.run(debug=True)
